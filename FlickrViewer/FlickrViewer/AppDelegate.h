af1420cc8c7558e2ad6d24fa048e27046e74daec
Oleksii Chopyk <alexei.chopyc@gmail.com>
2017-12-18 19:10:57 +0100

    Initial commit

---
+++ FlickrViewer/FlickrViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrViewer
+//
+//  Created by Oleksii Chopyk on 12/18/17.
+//  Copyright © 2017 Oleksii Chopyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrViewer/FlickrViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrViewer
+//
+//  Created by Oleksii Chopyk on 12/18/17.
+//  Copyright © 2017 Oleksii Chopyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
